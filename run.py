e443e9e6de66a5b1a6582f081e85947891f4df04
Atul Srivastava <asrivastava@scitec.com>
2020-05-10 16:26:44 -0400

    added some addtional html

--- run.py
+++ run.py
@@ -1,6 +1,43 @@
-from flask import Flask
+import datetime
+import time
+
+
+import io
+import json
+import os
+
+from flask import Flask, render_template, request, url_for, redirect
+
+
+try:
+    app.config['GA_TRACKING_ID'] = os.environ['GA_TRACKING_ID']
+except:
+    print('Tracking ID not set')
+
+
 app = Flask(__name__)
 
+
+resume_pdf_link = 'https://drive.google.com/open?id=0B2BrrDjIiyvmcWp5T194cy00UmM'
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    age = int((datetime.date.today() - datetime.date(1995, 4, 22)).days / 365)
+    return render_template('home.html', age=age)
+
+
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html', resume_pdf_link=resume_pdf_link)
+
+@app.route('/projects')
+def projects():
+    data = get_static_json("static/projects/projects.json")['projects']
+    data.sort(key=order_projects_by_weight, reverse=True)
+
+    tag = request.args.get('tags')
+    if tag is not None:
+        data = [project for project in data if tag.lower() in [project_tag.lower() for project_tag in project['tags']]]
+
+    return render_template('projects.html', projects=data, tag=tag)
